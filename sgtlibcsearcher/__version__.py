828ed56fff7edaf5befa557f5d7b3a1e4d74b994
汉广 <serfend@foxmail.com>
2022-06-05 11:45:22 +0800

    doc[version]update description

--- sgtlibcsearcher/__version__.py
+++ sgtlibcsearcher/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "sgtlibc"
-__description__ = f"for search version of libc.you can use like:`{__title__} puts:aa0+read:140 --dump system binsh` or in python , like : `py:import {__title__};s = {__title__}.LibcSearcher();s.add_condition('puts',0xaa0)`"
+__description__ = f"a offline python-lib for search libc function.for search version of libc.you can use like:`{__title__} puts:aa0+read:140 --dump system binsh` or in python , like : `py:import {__title__};s = {__title__}.LibcSearcher();s.add_condition('puts',0xaa0)`"
 __keywords__ = ['libcsearcher']
 __url__ = "https://github.com/serfend/sgtlibc"
 __version__ = "1.0.14"
