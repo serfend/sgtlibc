e4c3773ba2bbd5631c6d0b86ec281e4474dd97d7
汉广 <serfend@foxmail.com>
2022-06-05 20:03:16 +0800

    feat[test]init

---
+++ sgtlibc/tests/test_pyc.py
@@ -0,0 +1,22 @@
+from typing import List
+import sgtlibc
+
+
+def start_dump_normal(functions: List = None):
+    s = sgtlibc.Searcher()
+    s.add_condition('puts', 0xe10)
+    s.add_condition('read', 0x550)
+    return s.dump(functions)
+
+
+def test_dump_normal():
+    result = start_dump_normal()
+    assert len(list(result)) > 3, 'result should be valid'
+
+
+def test_dump_normal_with_reference():
+    r = ['system', 'str_bin_sh']
+    result = start_dump_normal(r)
+    assert len(list(result)) == 2, 'result should be same'
+    for i in r:
+        assert i in result, f'result should have function:{i}'
