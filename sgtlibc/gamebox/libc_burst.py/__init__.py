ac56e824cb5b39fa3dec71aa64393cb338b1412c
汉广 <serfend@foxmail.com>
2022-06-07 22:09:36 +0800

    feat[libc-burst]init

--- sgtlibc/gamebox/libc_burst.py/__init__.py
+++ sgtlibc/gamebox/libc_burst.py/__init__.py
@@ -2,17 +2,29 @@ from typing import Callable
 from ..client import GameBoxConfig
 from ...LibcSearcher import LibcSearcher
 from ... import logger, gamebox as gb
+from ...gamebox import ELF
 
 
 class LibcBursterExp:
-    def __init__(self, callback: Callable):
+    def __init__(self, leak_method: Callable, exp_before_leak: Callable, exp_after_leak: Callable):
         '''
-        callback: Callable[libc_dump_dict]
+        leak_method: Callable[function_name:str] -> int
+        exp_before_leak: Callable -> None
+        exp_after_leak: Callable[libc_dump_dict:dict[str:int]] -> None
         '''
-        self.callback = callback
+        self.leak_method = leak_method
+        self.exp_before_leak = exp_before_leak
+        self.exp_after_leak = exp_after_leak
+
+    def start_leak(self, default_function: str):
+        # TODO implement it
+        if self.exp.exp_before_leak:
+            self.exp.exp_before_leak()
+        self.exp.leak_method()
 
 
 class LibcBurster:
+    # TODO implement it
     '''
     you can input your payload for burster
     with libc-database search result
@@ -27,10 +39,11 @@ class LibcBurster:
         name = self.searcher.db[index]
         logger.debug(f'try libc[{index}] {name}')
         gb.set_config(self.config)
-        libc_data = self.searcher.dump(db_index = index)
-        
+
+        libc_data = self.searcher.dump(db_index=index)
 
     def run(self):
+        self.start_leak()
         for index, name in enumerate(self.searcher.db):
             try:
                 self.run_single(index)
