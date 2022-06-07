f6cbe61e1e9928127322a9ccc508d8a687f1ef63
汉广 <serfend@foxmail.com>
2022-06-07 14:55:43 +0800

    feat[test]support auto-offset

--- tests/test_search_result_offset.py
+++ tests/test_search_result_offset.py
@@ -1,13 +1,5 @@
 import sgtlibc
 
-
-def test_check_offset_status():
-    s = sgtlibc.Searcher()
-    s.add_condition('puts', 0xaa0)
-    s.dump(['puts'])
-    assert s.get_address('system') == False, 'should set offset first'
-
-
 def test_auto_load_function():
     s = sgtlibc.Searcher()
     s.add_condition('puts', 0xaa0)
