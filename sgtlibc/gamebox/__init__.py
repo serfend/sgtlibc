0a7bed9d433e4041ee23724152872eead6043719
汉广 <serfend@foxmail.com>
2022-06-08 11:20:27 +0800

    fix[gamebox]elf should not direct call

--- sgtlibc/gamebox/__init__.py
+++ sgtlibc/gamebox/__init__.py
@@ -2,4 +2,4 @@ from pwn import *
 from . import client
 from .control import *
 from .convert import *
-from .client import set_config, GameBoxConfig, start_game, elf
+from .client import set_config, GameBoxConfig, start_game
