f29ffc1efff87e26ce630aa6becd70706ed5d8d2
汉广 <serfend@foxmail.com>
2022-06-21 15:17:29 +0800

    feat[global]support directly use

--- sgtlibc/__init__.py
+++ sgtlibc/__init__.py
@@ -2,6 +2,8 @@ from sgtpyutils.logger import logger
 from .main import run, update_database as update
 from .LibcSearcher import LibcSearcher as Searcher
 from .LibcSearcher.commons import str_bin_sh as s_binsh, system as s_system, libc_start_main as s_libcstart, write as s_write, puts as s_puts, read as s_read, printf as s_printf
+
+from .gamebox import *
 from . import __version__, shellcraft, ROPgadgets, utils, FormatStr, constants
 logger.debug(
     f'{__version__.__title__} start, version:{__version__.__version__}')
