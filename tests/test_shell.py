4c65daaec341a663ff355c8d0d4644f5acb05066
汉广 <serfend@foxmail.com>
2022-06-22 22:20:28 +0800

    fix[test]remove shellcraft

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -5,4 +5,5 @@ def test_shellcode():
     assert sgtlibc.shellcraftex.shellcode64() != None
     assert sgtlibc.shellcraftex.shellcode86() != None
     assert sgtlibc.shellcraft.sh() != None
-    assert sgtlibc.shellcraftex.sh() != None
+    # user should direct use shellcraft for its behavior base on user call-function-name dynamically
+    # assert sgtlibc.shellcraftex.sh() != None
